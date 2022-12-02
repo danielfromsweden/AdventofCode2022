8b22e5e1eefb6721258e534ad084d2001762aca1
Daniel Bladh <daniel.blaaadh@gmail.com>
2022-12-02 08:30:18 +0100

    Day 1.1 Done

--- day1.py
+++ day1.py
@@ -1,15 +1,27 @@
 print('Day 1:')
 input_file = open("input.txt", "r")
-input = input_file.read()
-data_list = input.split("\n")
+data = input_file.read()
+data_list = data.split("\n")
 elf_sum = 0
 max_sum = 0
+elf_list = []
+#print(data_list)
+count = 0
+#print(len(data_list))
 for elf_calories in data_list:
-    data_list_calories = elf_calories.split(" ")
-    if data_list_calories[0] != "":
-        elf_sum += int(data_list_calories[0])
+    count += 1
+    #print(count)
+    #print(elf_calories)
+    if elf_calories != '':
+        elf_calories = int(elf_calories)
+        elf_sum += elf_calories
     else:
-        if elf_sum > max_sum:
-            max_sum = elf_sum
+        elf_list.append(elf_sum)
         elf_sum = 0
-print("Max sum is", max_sum)
+    if(count == len(data_list)):
+        elf_list.append((elf_sum))
+elf_list.sort(reverse=True)
+#print(elf_list)
+print("Max sum is", elf_list[0])
+print("The top 3 calories are", elf_list[0:3] )
+print("The total of top 3 is: ", elf_list[0]+elf_list[1]+elf_list[2])
