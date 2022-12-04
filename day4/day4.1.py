334fb16434cc79ab8668582e63b86f9fda491e11
danielfromsweden <daniel.blaaadh@gmail.com>
2022-12-04 21:20:40 +0100

    Day4.2 Started(ish)

--- day4/day4.1.py
+++ day4/day4.1.py
@@ -7,6 +7,7 @@ for line in Lines:
     numbers = [(line[0]).split('-'), (line[1]).split('-')]
     first_list = list(range(int(numbers[0][0]), int(numbers[0][1])+1))
     second_list = list(range(int(numbers[1][0]), int(numbers[1][1])+1))
+    list = [elem for elem in numbers]
     result_1_to_2 = all(elem in first_list  for elem in second_list)
     result_2_to_1 = all(elem in second_list  for elem in first_list)
     if result_1_to_2 or result_2_to_1:
