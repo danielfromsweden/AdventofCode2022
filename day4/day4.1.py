2eee41784738227d3591247375259ae53db1db5e
danielfromsweden <daniel.blaaadh@gmail.com>
2022-12-04 14:52:11 +0100

    Day4.1 Done

---
+++ day4/day4.1.py
@@ -0,0 +1,14 @@
+import string
+Lines = open("D:\Personal-Git\AdventofCode2022\day4\input.txt", 'r').read().splitlines()
+count = 0
+
+for line in Lines:
+    line = line.split(',')
+    numbers = [(line[0]).split('-'), (line[1]).split('-')]
+    first_list = list(range(int(numbers[0][0]), int(numbers[0][1])+1))
+    second_list = list(range(int(numbers[1][0]), int(numbers[1][1])+1))
+    result_1_to_2 = all(elem in first_list  for elem in second_list)
+    result_2_to_1 = all(elem in second_list  for elem in first_list)
+    if result_1_to_2 or result_2_to_1:
+        count += 1
+print("The number of fully contained pairs are:", count)
